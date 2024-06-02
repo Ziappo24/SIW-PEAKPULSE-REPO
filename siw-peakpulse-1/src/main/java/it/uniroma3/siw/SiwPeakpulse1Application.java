810b6fa07ac8c138430c4aab86c9524f996454ae
DESKTOP-PCOIPLV\EDOARDO <edo.piazzolla@stud.uniroma3.it>
2024-06-02 12:28:42 +0200

    è stato ricreato il progetto con maven invece che con gradle

--- siw-peakpulse-1/src/main/java/it/uniroma3/siw/SiwPeakpulse1Application.java
+++ siw-peakpulse-1/src/main/java/it/uniroma3/siw/SiwPeakpulse1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SiwPeakpulseApplication {
+public class SiwPeakpulse1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SiwPeakpulseApplication.class, args);
+		SpringApplication.run(SiwPeakpulse1Application.class, args);
 	}
 
 }

84478848443b519f3bb8a1efa45d54c5e70fe6cf
LAPTOP-GPD32DD8\utente <vincenzomilillo2002@gmail.com>
2024-06-23 12:36:08 +0200

    fix generico

--- siw-peakpulse-1/src/main/java/it/uniroma3/siw/controller/AttivitaController.java
+++ siw-peakpulse-1/src/main/java/it/uniroma3/siw/controller/AttivitaController.java
@@ -39,8 +39,8 @@ import jakarta.validation.Valid;
 @Controller
 public class AttivitaController {
 
-	private static String UPLOAD_DIR = "C:\\Users\\EDOARDO\\Desktop\\FOR SISW\\siw-peakpulse-repo\\siw-peakpulse-1\\src\\main\\resources\\static\\images";
-//	private static String UPLOAD_DIR = "C:\\Users\\utente\\Desktop\\UNIR3\\TERZO ANNO\\II SEMESTRE\\SISW\\siw-peakpulse-repo\\siw-peakpulse-1\\src\\main\\resources\\static\\images";
+//	private static String UPLOAD_DIR = "C:\\Users\\EDOARDO\\Desktop\\FOR SISW\\siw-peakpulse-repo\\siw-peakpulse-1\\src\\main\\resources\\static\\images";
+	private static String UPLOAD_DIR = "C:\\Users\\utente\\Desktop\\UNIR3\\TERZO ANNO\\II SEMESTRE\\SISW\\siw-peakpulse-repo\\siw-peakpulse-1\\src\\main\\resources\\static\\images";
 //	private static String UPLOAD_DIR = "C:\\Users\\UTENTE\\Documents\\workspace-spring-tool-suite-4-4.22.0.RELEASE\\siw-peakpulse-repo\\siw-peakpulse-1\\src\\main\\resources\\static\\images";
 	
 	@Autowired 
@@ -356,7 +356,7 @@ public class AttivitaController {
 	public String deleteAttivitaAdmin(@PathVariable("attivitaId") Long attivitaId, Model model) {
 		Attivita attivita = attivitaService.findById(attivitaId);
 		attivita.setAttrezzatureUtilizzate(null);
-		attivitaService.deleteById(attivita.getId());
+		attivitaService.deleteById(attivitaId);
 		return "redirect:/admin/manageAttivita";
 	}
 
